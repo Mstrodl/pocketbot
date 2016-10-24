3a8fb701f751c2ec1289549ca0b1a6fb7e88d852
Marius Ioan Orban <marius.orban99@gmail.com>
2016-10-24 22:22:55 +0300

    Fixed me forgetting how to JavaScript

--- core/logger.js
+++ core/logger.js
@@ -3,12 +3,14 @@ var chalk = require('chalk');
 
 var logFile = 'log.txt';
 
-module.exports.MESSAGE_TYPE = {
-    Info = 0,
-    OK = 1,
-    Warn = 2,
-    Error = 3
+var MESSAGE_TYPE = {
+    Info: 0,
+    OK: 1,
+    Warn: 2,
+    Error: 3
 };
+
+module.exports.MESSAGE_TYPE = MESSAGE_TYPE;
 
 module.exports.clearLogFile = function(){
     fs.writeFileSync(logFile, "");
@@ -18,24 +20,30 @@ module.exports.log = function(msg, type=0){
     var final_msg = "";
 
     switch(type){
-        case MESSAGE_TYPE.Info:{
-            final_msg = '[INFO]' + msg;
-        }
         case MESSAGE_TYPE.OK:{
-            final_msg = chalk.green('[OK]' + msg);
+            final_msg = chalk.green('[OK] ' + msg);
+            break;
         }
         case MESSAGE_TYPE.Warn:{
-            final_msg = chalk.yellow('[WARN]' + msg);
+            final_msg = chalk.yellow('[WARN] ' + msg);
+            break;
         }
         case MESSAGE_TYPE.Error:{
-            final_msg = chalk.red('[ERROR]' + msg);
+            final_msg = chalk.red('[ERROR] ' + msg);
+            break;
+        }
+        case MESSAGE_TYPE.Info:
+        default:{
+            final_msg = '[INFO] ' + msg;
         }
     }
 
     console.log(final_msg);
     
     fs.appendFile(logFile, final_msg, function(err){
-        //Error logging threw an error. Panic
-        console.log(chalk.red('[ERROR] Failed to log error:\n' + err));
+        if(err){
+            //Error logging threw an error. Panic
+            console.log(chalk.red('[ERROR] Failed to log error:\n' + err));
+        }
     });
 }
