e9c6333c02aad7f75c345e492bf165902991249a
mj0331 <marius.orban99@gmail.com>
2016-10-27 12:03:53 +0300

    /core/helper now uses the module system

--- core/helpers.js
+++ core/helpers.js
@@ -5,7 +5,7 @@
 // ! -- Feel free to improve any of these functions
 
 // Count function
-function getCount(arr, val) {
+module.exports.getCount = function(arr, val) {
 	let i, j, count = 0;
 	for (i=0,j=arr.length;i<j;i++) {
 		(arr[i] === val) && count++;
@@ -14,7 +14,7 @@ function getCount(arr, val) {
 }
 
 // Command timer
-function popCommand() {
+module.exports.popCommand = function() {
 	if (command.length>0)  command.shift();
 	//console.log(command);
 	setTimeout( function() {
@@ -23,10 +23,49 @@ function popCommand() {
 }
 
 // Get player ID
-function getUser(txt) {
+module.exports.getUser = function(txt) {
 	if (txt.match(/\b\d{10,}\b/g)) {
 		return txt.match(/\b\d{10,}\b/g)[0];
 	} else {
 		return undefined;
 	}
+}
+
+//Message argument parser
+module.exports.getArgs = function(message)
+{
+    //Split the input by spaces
+    var word_array = message.split(" ");
+    var args_array = [];
+    var temp_arg = "";
+
+    for(var key = 0; key < word_array.length; key++)
+    {
+        //If an element start with quotes, it's the start of a string'
+        if(word_array[key][0] == "\"" || word_array[key][0] == "\'")
+        {
+            var found_end = false;
+
+            //Search for the end of the string and also inceremnt the key counter
+            for(var j = key; j < word_array.length && !found_end; j++, key++)
+            {
+                //Add the word to the argument
+                temp_arg += word_array[j] + " ";
+
+                //If there's another quote at the end, we've reached the end of the argument
+                if(word_array[j][word_array[j].length - 1] == "\"" || word_array[j][word_array[j].length - 1] == "\'")
+                {
+                    found_end = true;
+                }
+            }
+            //Add the final argument to the argument array
+            args_array.push(temp_arg.substring(1, temp_arg.length-2));
+        }
+        else
+        {
+            args_array.push(word_array[key]);
+        }
+    }
+
+    return args_array;
 }
