7c16b42617d377a18723d24505024ee7dd5b54d1
mj0331 <marius.orban99@gmail.com>
2016-11-12 10:51:18 +0200

    Removed the PM message
    It's redundant

--- cmds/basic.js
+++ cmds/basic.js
@@ -36,12 +36,6 @@ var cmd_help = new command('ryionbot', '!help', 'Help command', function(data){
         to      : data.userID,
         message : helpText
     });
-    if (!(data.channelID in data.bot.directMessages)) {
-        data.bot.sendMessage({
-            to      : data.channelID,
-            message : `I have messaged you the full list of commands, <@${data.userID}> :thumbsup:`
-        });
-    }
 });
 
 module.exports.commands = [cmd_ping, cmd_ding, cmd_help];
