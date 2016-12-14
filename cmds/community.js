23c1afcfa068f99b8a6ee7da711b6dc094b7e20a
Brian Franco <mastastealth+git@gmail.com>
2016-12-14 01:36:08 +0000

    Small formatting adjustment

--- cmds/community.js
+++ cmds/community.js
@@ -81,10 +81,13 @@ let cmdKillDancer = new command('community', '!killdancer', `Kills any dancers H
 let cmd8Ball = new command('community', '!8ball', `Asks the 8ball a question`, function(data) {
     
     let x = Math.floor(Math.random()*(20-1+1)-1);
-    let answer = ["It is certain","It is decidedly so","Without a doubt","Yes, definitely","As I see it, yes","Most likely",
-    "Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now",
-    "Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no",
-    "Outlook not so good","Very doubtful"];
+    let answer = [
+        "It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
+        "As I see it, yes", "Most likely", "Outlook good", "Yes",
+        "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
+        "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
+        "My sources say no", "Outlook not so good", "Very doubtful"
+    ];
     
     dio.say(answer[x],data);
 });
